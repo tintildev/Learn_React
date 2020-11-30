bc40efb76dc92fd969da9e50d4c6d633fb5c1b72
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2020-11-30 21:19:51 +0100

    let vs const

---
+++ JS refreshing/let_vs_const.js
@@ -0,0 +1,24 @@
+//Old JS
+var myName = 'Max';
+console.log(myName);
+
+myName = 'Manu';
+console.log(myName);
+
+//Let - let allows you to declare variables whose scope is limited to the block, command or expression in which they are declared.
+let myName = 'Max';
+console.log(myName);
+
+myName = 'Manu';
+console.log(myName);
+
+//Const - Constants are block-scoped, much like variables declared using the let keyword. 
+var myName = 'Max';
+console.log(myName);
+
+/*
+Error
+myName = 'Manu';
+console.log(myName);
+*/
+
