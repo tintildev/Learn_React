3c0a18cfd2f434ed39312baa44547261fabc0de7
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2020-12-03 10:37:42 +0100

    jsx restrictions
    Elements need a root element.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,16 +5,18 @@ class App extends Component {
   render() {
 
     //It looks like HTML but is JSX code.
-    /*
+    
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
+        <p>I need a root element</p>
       </div> 
     );
-    */
+    
 
-  /* Understanding JSX - Our code will be converted into that*/
+  /* Understanding JSX - Our code will be converted into that
    return React.createElement('div', null, React.createElement('h1', {className: 'App'} , 'Do this work now?'));
+  */
   }
 }
 
