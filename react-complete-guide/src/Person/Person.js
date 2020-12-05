99fc8c4143a631ad816b218868cb85947bf89a06
martinklestil <Martin.klestil@gmx.at>
2020-12-05 17:21:06 +0100

    js code in jsx

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const person = () => {
-    return <p>I'm a Person!</p>
+    //JS Code in JSX
+    return <p>I'm a Person and I am {Math.floor(Math.random() *30 )}!</p>
 };
 
 export default person
