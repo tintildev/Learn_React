582de27fb2dcfba97e859bc3f8dc5991ed4de0de
martinklestil <Martin.klestil@gmx.at>
2020-12-06 16:30:36 +0100

    overview of state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,17 +3,28 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
-  render() {
+  /*State - React components has a built-in state object. 
+  The state object is where you store property values that belongs to the component. 
+  When the state object changes, the component re-renders.*/
+  
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Martin', age: 32},
+      {name: 'Lisa', age: 31},
+    ]
+  }
 
+  render() {
     //It looks like HTML but is JSX code.
-    
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="Max" age="28" />
-        <Person name="Martin" age="32">My Hobbies: Coding</Person>
-        <Person name="Lisa" age="31"/>
+        <buttun>Switch Name</buttun>
+        <Person name= {this.state.persons[0].name} age= {this.state.persons[0].age} />
+        <Person name= {this.state.persons[1].name} age= {this.state.persons[1].age}>My Hobbies: Coding</Person>
+        <Person name= {this.state.persons[2].name} age= {this.state.persons[2].age}/>
       </div> 
     );
     
