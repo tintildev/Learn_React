90ecaabeaa9db79c546e4ea22f517073af362d12
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2020-12-19 16:14:37 +0100

    add Styling

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Person.css'
 
 const person = (props) => {
     //JS Code in JSX
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm {props.name} and I am {props.age}!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name} />
