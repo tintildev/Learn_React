5af8f6f29714c360abd487240ded6dc4d98c0778
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2020-12-30 06:47:28 +0100

    using radium for media queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person'
 
 class App extends Component {
@@ -125,6 +125,7 @@ class App extends Component {
 
     //It looks like HTML but is JSX code.
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
@@ -135,6 +136,7 @@ class App extends Component {
         {persons}
 
       </div> 
+      </StyleRoot>
     );
     
 
