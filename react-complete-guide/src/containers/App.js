412624ca53365b4f7ff05f3e717d06e3428eeefd
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-01-01 21:31:03 +0100

    Class-based vs Functional Components

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -78,6 +78,7 @@ class App extends Component {
     return (
       <div className="App">  
         <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons} 
           clicked={this.togglePersonsHandler}/>
