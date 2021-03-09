0c421bd83b94dcda7fc01bf92a7d180cf04310be
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-03-09 05:30:16 +0100

    finish footer

--- my-helloworld/src/components/Footer.js
+++ my-helloworld/src/components/Footer.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import '../css/Footer.css';
+import logo from '../img/logo-mk.png';
 
 function Footer() {
     return (
-        <div class="footer">
-
-            <div class="footer__logo-box">
-                
+        <div className="footer">
+            <div className="footer__links">
+                    <a href="https://www.linkedin.com/in/martin-klestil/" target="_blank" rel="noreferrer" className="fa fa-linkedin">Linkedin</a>
+                    <a href="https://www.instagram.com/mklestil/" target="_blank" rel="noreferrer" className="fa fa-instagram">Instagram</a>
+                    <a href="https://twitter.com/mklestil" target="_blank" rel="noreferrer" className="fa fa-twitter">Twitter</a>
             </div>
-            <div class="footer__copyright">
-                © 2021 Martin Klestil        
-    
-                <div class="footer__links">
-                    <a href="https://facebook.com/feuerwehrMichelhausen/" class="fa fa-facebook"></a>
-                    <a href="https://www.instagram.com/feuerwehrmichelhausen/" class="fa fa-instagram"></a>
-                    <a href="https://twitter.com/ffmichelhausen" class="fa fa-twitter"></a>
-                </div>
+            <div className="footer__logo-box">
+                <img src={logo} alt="Logo" className="footer__LogoImg"/>
+            </div>
+            <div className="footer__copyright">
+                <hr></hr>
+                © 2021 Martin Klestil
+                <hr></hr>
             </div>
         </div>      
     )
--- my-helloworld/src/components/Header.js
+++ my-helloworld/src/components/Header.js
@@ -4,14 +4,14 @@ import '../css/Header.css';
 
 function Header() {
     return (
-        <div class="header">
+        <div className="header">
             <header>
             
-            <h3 class="heading-3">Hi, my name is</h3>
-            <h1 class="heading-1 heading--main">MARTIN KLESTIL.</h1>
-            <h1 class="heading-1 heading--main">I love to code.</h1>
-            <h4 class="heading-4 heading--sub">I'm a software engineer based in Tulln, Austria. Here you can see my path to becoming a web developer.</h4>
-            <button class="btn btn__btn--animated">My portfolio</button>   
+            <h3 className="heading-3">Hi, my name is</h3>
+            <h1 className="heading-1 heading--main">MARTIN KLESTIL.</h1>
+            <h1 className="heading-1 heading--main">I love to code.</h1>
+            <h4 className="heading-4 heading--sub">I'm a software engineer based in Tulln, Austria. Here you can see my path to becoming a web developer.</h4>
+            <button className="btn btn__btn--animated">My portfolio</button>   
             </header>
         </div>
     )
