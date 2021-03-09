1026362059c37e13e2c2084c437056da9e39fbdc
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-03-09 04:45:52 +0100

    add footer my-helloworld

---
+++ my-helloworld/src/components/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../css/Footer.css';
+
+function Footer() {
+    return (
+        <div class="footer">
+
+            <div class="footer__logo-box">
+                
+            </div>
+            <div class="footer__copyright">
+                © 2021 Martin Klestil        
+    
+                <div class="footer__links">
+                    <a href="https://facebook.com/feuerwehrMichelhausen/" class="fa fa-facebook"></a>
+                    <a href="https://www.instagram.com/feuerwehrmichelhausen/" class="fa fa-instagram"></a>
+                    <a href="https://twitter.com/ffmichelhausen" class="fa fa-twitter"></a>
+                </div>
+            </div>
+        </div>      
+    )
+}
+export default Footer
