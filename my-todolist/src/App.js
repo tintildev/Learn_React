e671ccc32392018067421f3a74155a0fadb03b6b
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-03-11 21:28:02 +0100

    start todolist

---
+++ my-todolist/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './components/TodoItem';
+import TodoItem from './components/TodoItem';
+
+function App() {
+  return (
+    <div className="App">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+    </div>
+  );
+}
+
+export default App;
