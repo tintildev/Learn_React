802b9586a5e9d558bd5a35889067753d5bfb3b6c
Martin Klestil <34619286+martinklestil@users.noreply.github.com>
2021-03-14 16:57:47 +0100

    add local storage

--- my-todo/src/App.js
+++ my-todo/src/App.js
@@ -1,33 +1,41 @@
 import './App.css';
 import TodoList from './components/TodoList';
-import React, { useState, useRef, Component} from 'react';
+import React, { useState, useRef, useEffect, Component} from 'react';
 import {v4 as uuid} from 'uuid';
 
 function App() {
   const [todos, setTodos] = useState([]);
-
   {/*  So that we can access the input field, we need useRef() */}
   const todoNameRef = useRef();
+  {/*  Local storage */}
+  const LOCAL_STORAGE_KEY = 'todoApp.todos';
 
-  
+  {/*  Local storage load */}
+  useEffect(() =>{
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if(storedTodos) setTodos(storedTodos)
+    console.log("loade")
+  }, [])
+
+  {/*  Local storage save */}
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    console.log("SAVE")
+  },[todos]);
+
 
   {/*Save the new state */}
   function handleAddTodo(e) {
-    console.log("Test")
     {/* Get the input */}
     const name = todoNameRef.current.value;
-
     {/* Checks the input */}
     if (name === '') return
-
       {/* Sets a new state with all old entries */}
       setTodos(prevTodos => {
         return [...prevTodos, { id: uuid(), name: name, complete: false}]
       })
-
        {/* Clears the input field */}
       todoNameRef.current.value = null;
-    
   }
 
   return (
