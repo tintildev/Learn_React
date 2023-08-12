d1d7bf2c847d0f5097507e026583c812e989d48c
martinklestil <Martin.klestil@gmx.at>
2023-08-12 13:08:14 +0200

    work on expenseItem
    add sass to project

--- expenses-tracker/src/components/ExpenseItem.js
+++ expenses-tracker/src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
-function ExpenseItem(){
-  return <h2>Expense item!</h2>
+import './sass/component/expenseItem.scss'
+
+function ExpenseItem() {
+  return (
+    <div class="expense-item">
+      <div class="expense-item__date">March 28th 2021</div>
+      <div class="expense-item__description">
+        <h2>Car Insurance</h2>
+      </div>
+      <div class="expense-item__price">294.67€</div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
