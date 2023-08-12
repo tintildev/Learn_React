7111d656405f89004a3fe721e0c9e62dc43fe244
martinklestil <Martin.klestil@gmx.at>
2023-08-12 14:39:27 +0200

    work with props
    add props to component

--- expenses-tracker/src/components/ExpenseItem.js
+++ expenses-tracker/src/components/ExpenseItem.js
@@ -1,16 +1,14 @@
 import './sass/component/expenseItem.scss'
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;
+function ExpenseItem(props) {
+  
   return (
     <div class="expense-item">
-      <div class="expense-item__date">{expenseDate.toISOString()}</div>
+      <div class="expense-item__date">{props.date.toISOString()}</div>
       <div class="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div class="expense-item__price">{expenseAmount} €</div>
+      <div class="expense-item__price">{props.amount} €</div>
     </div>
   );
 }
