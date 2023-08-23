3d848cb45cee0144015e75123f7c24e63ff96da9
Martin Klestil <34619286+mklestil@users.noreply.github.com>
2023-08-23 19:03:21 +0200

    add new expenses component

--- expenses-tracker/src/App.js
+++ expenses-tracker/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 function App() {
   return (
     <div className="App">
+      <NewExpense />
       <Expenses></Expenses>
     </div>
   );
